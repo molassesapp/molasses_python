40c179fb9667192fbd580dd629491c652f2c07cf
James Hrisho <james.hrisho@gmail.com>
2021-02-14 21:51:56 -0500

    Update versions

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/molassesapp/molasses_python',
-    version='0.1.3',
+    version='0.3.1',
     zip_safe=False,
 )
